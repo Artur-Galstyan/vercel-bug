39a3148b347f886b4c79cff6569365552656dd8a
Artur Galstyan <mail@arturgalstyan.dev>
2025-06-05 00:22:18 +0200

    wtf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import tailwindcss from "@tailwindcss/vite";
 import vercelStatic from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+    shikiConfig: {
+      themes: { light: "min-light", dark: "night-owl" },
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
   adapter: vercelStatic({
     webAnalytics: {
       enabled: true,
